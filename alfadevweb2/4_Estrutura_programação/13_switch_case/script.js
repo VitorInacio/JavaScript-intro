ba4b805dbc1bcc182b4a6772532b0f61ff45ea32
VitorInacio <110571732+VitorInacio@users.noreply.github.com>
2022-08-24 22:12:45 -0300

    Add files via upload

---
+++ alfadevweb2/4_Estrutura_programação/13_switch_case/script.js
@@ -0,0 +1,12 @@
+nome = "Denicreidisson";
+
+switch(nome) {
+    case "Denicreidisson":
+        console.log("Bem vindo Denicreidisson");
+        break;
+    case "João":
+        console.log("Bem vindo João");
+        break;
+    default:
+        console.log("Nome não encontrado!");
+}
